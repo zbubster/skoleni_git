15293cc0891516888a1eeec0a1a489d2913623be
zbubster <zbu.rataj@seznam.cz>
2025-05-14 13:33:59 +0200

    pridavam skript

---
+++ script.R
@@ -0,0 +1,7 @@
+# skoleni git/github
+
+usethis::use_git() #zahaji version control pro tento projekt
+
+usethis::create_project("xxx_dir_xxx") #vytvari novy projekt s gitem aktivovanym
+usethis::use_git()
+
