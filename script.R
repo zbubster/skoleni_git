f5a2d307879de78d7dc5ecd94e267a967a2693a4
zbubster <zbu.rataj@seznam.cz>
2025-05-14 14:25:43 +0200

    finalizace kodu pro pripojeni repa na cloudy, delece redundantniho skriptu

--- script.R
+++ script.R
@@ -6,5 +6,9 @@ usethis::create_project("xxx_dir_xxx") #vytvari novy projekt s gitem aktivovanym
 usethis::use_git()
 
 # inicializace github repository
-usethis::use_github()
+usethis::use_git_config(user.name = "zbubster", user.email = "zbu.rataj@seznam.cz")
+usethis::create_github_token() #vygeneruje token
+# token tu nemuze byt, secret by slo ven
+gitcreds::gitcreds_set() #nastaveni credentials, bude to chtit PAT token
 
+usethis::use_github() #tohle vytvori to repo na cloudu
